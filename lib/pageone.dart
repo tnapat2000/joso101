d24c9cbb90e29f62e5aa4e6f53e1e20b463196b8
litanitchanun <praew301244@gmail.com>
2022-07-08 03:52:45 +0700

    welcome pages (first milestone)

--- lib/pageone.dart
+++ lib/pageone.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'colors.dart';
 
 class PageOne extends StatelessWidget {
   const PageOne({Key? key}) : super(key: key);
@@ -6,20 +7,51 @@ class PageOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blueGrey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset("assets/images/accidents.png"),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              "Play with the data",
-              style: TextStyle(color: Colors.purpleAccent, fontSize: 36),
-            ),
-          )
-        ],
-      ),
+      color: background_green,
+        child: Center(
+            child: Container(
+              width: 300,
+              height: 300,
+              padding: new EdgeInsets.all(10.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                color: light_green,
+                elevation: 10,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      subtitle: Text(
+                          'Hello,\n\nI am your trip assistant. Car accidents occur frequently in Bangkok, Thailand.\n\nI will let you know if you are in the area where accidnets may occur.\n\nTutotial is in the next page.',
+                          style: TextStyle(fontSize: 18.0)
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            )
+        )
+
     );
+
+//   Container(
+    //   color: background_green,
+    //   child: Column(
+    //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //     children: [
+    //       Image.asset("assets/images/accidents.png"),
+    //       Padding(
+    //         padding: EdgeInsets.all(20),
+    //         child: Text(
+    //           "Play with the data",
+    //           style: TextStyle(color: Colors.purpleAccent, fontSize: 36),
+    //         ),
+    //       )
+    //     ],
+    //   ),
+    // );
   }
 }
--- lib/pagethree.dart
+++ lib/pagethree.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'colors.dart';
 
 class PageThree extends StatelessWidget {
   const PageThree({Key? key}) : super(key: key);
@@ -6,18 +7,86 @@ class PageThree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blueGrey,
-      child: Column(
+      color: background_green,
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset("assets/images/report.png"),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              "Interact with it",
-              style: TextStyle(color: Colors.purpleAccent, fontSize: 36),
+        children: <Widget>[
+          Container(
+            // color: Colors.blue,
+            // height: 100,
+            // width: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/rescan.png',
+                  height: 100,
+                ),
+                Image.asset(
+                  'assets/images/recenter.png',
+                  height: 100,
+                ),
+                Image.asset(
+                  'assets/images/report_accidents.png',
+                  height: 100,
+                ),
+                Image.asset(
+                  'assets/images/pin_accident.png',
+                  height: 100,
+                ),
+              ],
             ),
-          )
+          ),
+          Container(
+            //   color: Colors.amber,
+            //   width: 100,
+            // height: 100
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  // Text(),
+                  child: Text(
+                    '"Rescan" lets you\nrefresh your\ndatabase again',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                Container(
+                  // Text(),
+                  child: Text(
+                    '"Recenter" changes\nthe user pin to the\ncenter of the map',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                Container(
+                  // Text(),
+                  child: Text(
+                    'You can report an\naccident here',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                Container(
+                  // Text(),
+                  child: Text(
+                    'You can pin the\nlocation the accident\noccurs and give\nmore information',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
