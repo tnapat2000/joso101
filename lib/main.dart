76cc2be875493d9d101f7298495542db325afaed
NetNapat2543 <tnapat2000>
2022-07-16 01:53:40 +0700

    "able to read data from firestore"

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:joso101/authen/login_screen.dart';
-import 'package:joso101/tutorial/myhomepage.dart';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'map/map_screen.dart';
@@ -8,6 +8,7 @@ import 'map/map_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   // comment it before launching
   final prefs = await SharedPreferences.getInstance();
--- lib/map/map_screen.dart
+++ lib/map/map_screen.dart
@@ -1,14 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
-// import 'package:joso101/LocData.dart';
 import 'package:latlong/latlong.dart';
-import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-// import 'package:provider/provider.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -26,6 +22,7 @@ class _MapScreenState extends State<MapScreen> {
   late Position currentLocation;
   LatLng currentPoint = LatLng(37.421871, -122.084122);
   late final MapController mapController;
+  late List<Marker> accidents;
 
   @override
   void initState() {
@@ -36,12 +33,12 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   void initFirebase() async {
-    await Firebase.initializeApp();
     _auth = FirebaseAuth.instance;
     _fstore = FirebaseFirestore.instance;
     //  for testing
     await _auth.signInWithEmailAndPassword(
         email: "tester1@gmail.com", password: "peepoo");
+
     currentUser = _auth.currentUser?.email ?? "none";
     print(currentUser);
   }
@@ -81,38 +78,6 @@ class _MapScreenState extends State<MapScreen> {
     mapController.move(LatLng(lat, long), 17.0);
   }
 
-  List<Marker> testMarkers = [
-    Marker(
-        width: 50,
-        height: 50,
-        point: LatLng(37.421, -122.089),
-        builder: (context) => const Icon(
-              Icons.location_on,
-              color: Colors.blue,
-              size: 50,
-            )),
-    Marker(
-        width: 50,
-        height: 50,
-        point: LatLng(37.422, -122.083),
-        builder: (context) => const Icon(
-          Icons.location_on,
-          color: Colors.blue,
-          size: 50,
-        )
-    ),
-    Marker(
-        width: 50,
-        height: 50,
-        point: LatLng(37.423, -122.084),
-        builder: (context) => const Icon(
-          Icons.location_on,
-          color: Colors.blue,
-          size: 50,
-        )
-    )
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -135,71 +100,183 @@ class _MapScreenState extends State<MapScreen> {
       body: SafeArea(
         child: Stack(
           children: [
-            StreamBuilder<Position>(
-                stream: getCurrentLocation(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
+            StreamBuilder<QuerySnapshot>(
+                stream: _fstore.collection('accidentsFromUser').snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    final snap = snapshot.data!.docs;
+                    return
+                        // ListView.builder(
+                        //   shrinkWrap: true,
+                        //   primary: false,
+                        //   itemCount: snap.length,
+                        //   itemBuilder: (context, index) {
+                        //     return Text(snap[index]['lat'].toString() +
+                        //         ' : ' +
+                        //         snap[index]['lng'].toString());
+                        //   },
+                        // );
+                        StreamBuilder<Position>(
+                            stream: getCurrentLocation(),
+                            builder: (context, snapshot) {
+                              if (!snapshot.hasData) {
+                                return const Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              }
+                              currentPoint = LatLng(snapshot.data?.latitude,
+                                  snapshot.data?.longitude);
+                              // Provider.of<LocData>(context, listen: false).changePos(point);
+                              return Column(
+                                children: [
+                                  // Center(
+                                  //   child: Column(
+                                  //     children: [
+                                  //       // Provider.of<LocData>(context, listen: false).changePosWidget(point),
+                                  //       Text(
+                                  //         "LAT :" +
+                                  //             currentPoint.latitude.toString(),
+                                  //         style: TextStyle(
+                                  //             color: Colors.pinkAccent,
+                                  //             fontSize: 25),
+                                  //       ),
+                                  //       Text(
+                                  //         "LNG :" +
+                                  //             currentPoint.longitude.toString(),
+                                  //         style: TextStyle(
+                                  //             color: Colors.purple,
+                                  //             fontSize: 25),
+                                  //       ),
+                                  //     ],
+                                  //   ),
+                                  // ),
+                                  Expanded(
+                                    child: FlutterMap(
+                                      mapController: mapController,
+                                      options: MapOptions(
+                                          center: currentPoint,
+                                          zoom: 18.0,
+                                          minZoom: 11.0,
+                                          maxZoom: 17.0,
+                                          interactiveFlags:
+                                              InteractiveFlag.pinchZoom |
+                                                  InteractiveFlag.drag,
+                                          plugins: [
+                                            MarkerClusterPlugin(),
+                                          ]),
+                                      layers: [
+                                        TileLayerOptions(
+                                            urlTemplate:
+                                                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                                            subdomains: ['a', 'b', 'c']),
+                                        MarkerLayerOptions(
+                                            markers: List.generate(
+                                                    snap.length,
+                                                    (index) => Marker(
+                                                        width: 50,
+                                                        height: 50,
+                                                        point: LatLng(
+                                                            snap[index]['lat'],
+                                                            snap[index]['lng']),
+                                                        builder: (context) =>
+                                                            const Icon(
+                                                              Icons.location_on,
+                                                              color:
+                                                                  Colors.blue,
+                                                              size: 50,
+                                                            ))) +
+                                                [
+                                                  Marker(
+                                                      width: 100.0,
+                                                      height: 100.0,
+                                                      point: currentPoint,
+                                                      builder: (context) =>
+                                                          const Icon(
+                                                            Icons.location_on,
+                                                            color: Colors
+                                                                .redAccent,
+                                                            size: 50,
+                                                          ))
+                                                ]),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              );
+                            });
+                  } else {
+                    return StreamBuilder<Position>(
+                        stream: getCurrentLocation(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          currentPoint = LatLng(snapshot.data?.latitude,
+                              snapshot.data?.longitude);
+                          // Provider.of<LocData>(context, listen: false).changePos(point);
+                          return Column(
+                            children: [
+                              Center(
+                                child: Column(
+                                  children: [
+                                    // Provider.of<LocData>(context, listen: false).changePosWidget(point),
+                                    Text(
+                                      "LAT :" +
+                                          currentPoint.latitude.toString(),
+                                      style: TextStyle(
+                                          color: Colors.pinkAccent,
+                                          fontSize: 25),
+                                    ),
+                                    Text(
+                                      "LNG :" +
+                                          currentPoint.longitude.toString(),
+                                      style: TextStyle(
+                                          color: Colors.purple, fontSize: 25),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: FlutterMap(
+                                  mapController: mapController,
+                                  options: MapOptions(
+                                      center: currentPoint,
+                                      zoom: 18.0,
+                                      minZoom: 11.0,
+                                      maxZoom: 17.0,
+                                      interactiveFlags:
+                                          InteractiveFlag.pinchZoom |
+                                              InteractiveFlag.drag,
+                                      plugins: [
+                                        MarkerClusterPlugin(),
+                                      ]),
+                                  layers: [
+                                    TileLayerOptions(
+                                        urlTemplate:
+                                            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                                        subdomains: ['a', 'b', 'c']),
+                                    MarkerLayerOptions(markers: [
+                                      Marker(
+                                          width: 100.0,
+                                          height: 100.0,
+                                          point: currentPoint,
+                                          builder: (context) => const Icon(
+                                                Icons.location_on,
+                                                color: Colors.redAccent,
+                                                size: 50,
+                                              ))
+                                    ]),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          );
+                        });
                   }
-                  currentPoint =
-                      LatLng(snapshot.data?.latitude, snapshot.data?.longitude);
-                  // Provider.of<LocData>(context, listen: false).changePos(point);
-                  return Column(
-                    children: [
-                      Center(
-                        child: Column(
-                          children: [
-                            // Provider.of<LocData>(context, listen: false).changePosWidget(point),
-                            Text(
-                              "LAT :" + currentPoint.latitude.toString(),
-                              style: TextStyle(
-                                  color: Colors.pinkAccent, fontSize: 25),
-                            ),
-                            Text(
-                              "LNG :" + currentPoint.longitude.toString(),
-                              style:
-                                  TextStyle(color: Colors.purple, fontSize: 25),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: FlutterMap(
-                          mapController: mapController,
-                          options: MapOptions(
-                              center: currentPoint,
-                              zoom: 18.0,
-                              minZoom: 11.0,
-                              maxZoom: 17.0,
-                              interactiveFlags: InteractiveFlag.pinchZoom |
-                                  InteractiveFlag.drag,
-                              plugins: [
-                                MarkerClusterPlugin(),
-                              ]),
-                          layers: [
-                            TileLayerOptions(
-                                urlTemplate:
-                                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                                subdomains: ['a', 'b', 'c']),
-                            MarkerLayerOptions(markers: testMarkers + [
-                              Marker(
-                                  width: 100.0,
-                                  height: 100.0,
-                                  point: currentPoint,
-                                  builder: (context) => const Icon(
-                                        Icons.location_on,
-                                        color: Colors.redAccent,
-                                        size: 50,
-                                      ))
-                            ]),
-                          ],
-                        ),
-                      ),
-                    ],
-                  );
-                })
+                }),
           ],
         ),
       ),
